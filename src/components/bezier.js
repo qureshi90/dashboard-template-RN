e61699d3bcf555d1f7d6d8859d140e54b08df06e
Waqar Qureshi <waqarqureshi90@gmail.com>
2021-03-17 12:38:19 +0500

    bezier curve added to analytics

---
+++ src/components/bezier.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {LineChart} from 'react-native-chart-kit';
+import {Dimensions} from 'react-native';
+const screenWidth = Dimensions.get('window').width;
+
+const chartConfig = {
+  backgroundGradientFrom: '#AFF',
+  // backgroundGradientFromOpacity: 1,
+  backgroundGradientTo: '#AFF',
+  // backgroundGradientToOpacity: 1,
+  color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+  strokeWidth: 2, // optional, default 3
+  // barPercentage: 0.5,
+  // useShadowColorFromDataset: false, // optional
+};
+
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+  datasets: [
+    {
+      data: [20, 45, 28, 80, 99, 43],
+      color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+      strokeWidth: 2, // optional
+    },
+  ],
+  legend: ['Rainy Days'], // optional
+};
+
+const Bezier = () => {
+  return (
+    <>
+      <LineChart
+        data={data}
+        width={screenWidth}
+        height={250}
+        // verticalLabelRotation={10}
+        chartConfig={chartConfig}
+        bezier
+      />
+    </>
+  );
+};
+
+export default Bezier;
--- src/screens/analytics/index.js
+++ src/screens/analytics/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {ScrollView} from 'react-native';
 import styles from './style.js';
 import Pie from '../../components/pieChart';
+import Bezier from '../../components/bezier';
 
 const Analytics = ({navigation}) => {
   return (
     <>
       <ScrollView style={styles.container}>
+        <Bezier />
         <Pie />
       </ScrollView>
     </>
