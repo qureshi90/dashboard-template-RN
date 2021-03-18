5c32ce1325c240c9b77a585e4dcf85e2d0dbb822
Waqar Qureshi <waqarqureshi90@gmail.com>
2021-03-18 13:53:20 +0500

    sidemenu added

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Navigator from './src/navigation/navigator.js';
+// import Navigator from './src/navigation/navigator.js';
+import Burger from './src/navigation/drawer.js';
 
 const App = () => {
   return (
     <>
-      <Navigator />
+      <Burger />
     </>
   );
 };
---
+++ src/navigation/drawer.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import SideMenu from 'react-native-side-menu';
+import {View, Text, StyleSheet, Animated} from 'react-native';
+
+class ContentView extends React.Component {
+  render() {
+    return (
+      <View style={styles.content}>
+        <Text>Welcome to React Native!</Text>
+        <Text>To get started, edit index.ios.js</Text>
+        <Text>Press Cmd+R to reload Cmd+Control+Z for dev menu</Text>
+      </View>
+    );
+  }
+}
+
+const Menu = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+      <Text>Dashboard</Text>
+      <Text>Orders</Text>
+      <Text>Notifications</Text>
+      <Text>Activity</Text>
+    </View>
+  );
+};
+
+class Application extends React.Component {
+  render() {
+    const menu = <Menu navigator={navigator} />;
+
+    return (
+      <SideMenu
+        menu={menu}
+        animationFunction={(prop, value) =>
+          Animated.spring(prop, {
+            toValue: value,
+            friction: 8,
+            useNativeDriver: true,
+          })
+        }>
+        <ContentView />
+      </SideMenu>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'cyan',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  content: {
+    flex: 1,
+    backgroundColor: 'blue',
+  },
+});
+
+export default Application;
+
+// import 'react-native-gesture-handler';
+// import React from 'react';
+// import {View, Text} from 'react-native';
+// // import Drawer from 'react-native-drawer';
+// // import {NavigationContainer} from '@react-navigation/native';
+// // import {createDrawerNavigator} from '@react-navigation/drawer';
+// // import Dashboard from '../screens/dashboard';
+// // import Orders from '../screens/orders';
+// // import Notifications from '../screens/notifications';
+// // import ActivityFeed from '../screens/activityFeed';
+
+// // const Drawer = createDrawerNavigator();
+
+// const Burger = () => {
+//   return (
+//     <>
+//       <View>
+//         <Text>Hello</Text>
+//       </View>
+//       {/* <Drawer
+//         type="static"
+//         content={<Orders />}
+//         tapToClose={true}
+//         openDrawerOffset={0.2}
+//         panCloseMask={0.2}>
+//         <Dashboard />
+//       </Drawer> */}
+//       {/* <NavigationContainer>
+//         <Drawer.Navigator initialRouteName="dashboard">
+//           <Drawer.Screen name="dashboard" component={Dashboard} />
+//           <Drawer.Screen name="orders" component={Orders} />
+//           <Drawer.Screen name="notifications" component={Notifications} />
+//           <Drawer.Screen name="activityFeed" component={ActivityFeed} />
+//         </Drawer.Navigator>
+//       </NavigationContainer> */}
+//     </>
+//   );
+// };
+
+// export default Burger;
